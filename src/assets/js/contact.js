cd219a021bb71ea19378ef3d90b15c8a7841b737
ifancyabroad <edgar.nightingale@fanhubmedia.com>
2020-11-29 13:56:30 +0000

    Add contact form code

---
+++ src/assets/js/contact.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+	// Variable to hold request
+    let request;
+
+	// Add submit event listener to the form
+	$("#contact-form").submit(function(e) {
+        e.preventDefault();
+
+         // Abort any pending request
+        if (request) {
+            request.abort();
+        }
+
+        const form = $(this);
+        const inputs = form.find("input, button, textarea");
+        const submitBtn = form.find("button[type=\"submit\"]");
+        const successMessage = $("#successMessage");
+        const errorMessage = $("#errorMessage");
+
+        // Show some response on the button
+        submitBtn.prop('type','button' );
+        submitBtn.prop('orig_label', submitBtn.text());
+        submitBtn.text('Sending ...');
+
+        // Serialise the form data
+        const serializedData = form.serialize();
+
+        // Let's disable the inputs for the duration of the Ajax request.
+        // Note: we disable elements AFTER the form data has been serialized.
+        // Disabled form elements will not be serialized.
+        inputs.prop('disabled', true);
+
+		// Send AJAX request
+		request = $.ajax({
+			type: 'POST',
+			url: '../handler.php',
+            data: serializedData,
+            dataType: 'json'
+        });
+        
+        // Display thank you message if request sent successfully
+	    request.done(function (response, textStatus, jqXHR){
+            form.hide();
+            successMessage.show();
+            errorMessage.hide();
+	    });
+
+	    // Inform user if message fails to send
+	    request.fail(function (jqXHR, textStatus, errorThrown){
+            inputs.prop("disabled", false);
+            successMessage.hide();
+            errorMessage.show();
+
+            // Reverse the response on the button
+            label = submitBtn.prop('orig_label');
+            if (label) {
+                submitBtn.prop('type','submit' );
+                submitBtn.text(label);
+                submitBtn.prop('orig_label','');
+            }
+        });
+    });
+});
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import './assets/scss/styles.scss';
 import 'slick-carousel';
+import './assets/js/contact.js';
 import './assets/js/slider.js';
 
